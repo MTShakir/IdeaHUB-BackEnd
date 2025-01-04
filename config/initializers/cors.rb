256ee9fa6afc4aa63101877deb019a6b11ab5a57
MTShakir <167106121+MTShakir@users.noreply.github.com>
2025-01-04 02:27:28 +0000

    Update cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,18 +1,15 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://coursework-frontend-hqri.vercel.app/',
-            'http://localhost:3000',
+    origins 'https://coursework-frontend-hqri.vercel.app',  # Frontend URLs
+            'http://localhost:3000',                      # Local development
             'https://ideahub-frontend.onrender.com',
             'https://idea-hub-front-end.vercel.app',
-            'https://idea-hub-front-end.vercel.app/',
             'https://idea-hub-front-end-git-main-mtshakirs-projects.vercel.app',
-            'https://idea-hub-front-end-git-main-mtshakirs-projects.vercel.app/',
-            'https://idea-hub-front-df9sqg2w1-mtshakirs-projects.vercel.app/'
-            
+            'https://idea-hub-front-df9sqg2w1-mtshakirs-projects.vercel.app'
 
-    resource '*',
-             headers: :any,
-             methods: [:get, :post, :put, :patch, :delete, :options],
-             credentials: true # Allow cookies or Authorization headers
+    resource '*',                                      # Allow all resources
+      headers: :any,                                     # Allow all headers
+      methods: [:get, :post, :put, :patch, :delete, :options],  # Allowed methods
+      credentials: true                                  # Allow cookies or Authorization headers
   end
 end
